7d0ec8815db7fba1f961e17ee72a3eea2848d668
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-04-20 01:10:45 +0400

    always commit

--- quiz.py
+++ quiz.py
@@ -6,50 +6,54 @@ import random
 import os
 correct_answer = 0
 
-
-dictSubtitle = YouTubeTranscriptApi.get_transcript('1ooqsxtdGS0') 
-generalText = " ".join(i['text'] for i in dictSubtitle)
-for i in dictSubtitle:
-   generalText += " " + i['text']
- 
-client = OpenAI()
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-  {"role": "system", "content": f"I am sending you a text.you MUST represent 10 quetion,the questions and answers as a list of dicts in Python in this way: like json file. without python``` and without numbering, only listof dicts: answers must did not have \"symbol \"qeuestion\",\"A\":answer,\"B\":answer,\"C\":answer,\"D\":answer, correct: same  answer which is correct: TEXT{generalText}"},
-    #{"role": "user", "content": f"{generalText}"}
-  ]
-)
-path_file = 'C:\Future\\for git\Quiz_Scribe\q.txt' # os path join
+video_id = st.text_input(" Send YouTube video Id")
+if st.button("next"):
+  file_path = video_id.join(".txt")
+  print("here",file_path)
+  
+  if  os.path.exists(file_path):
+    with open(file_path, "r") as f:
+      text = f.read()
+  else:
+      dictSubtitle = YouTubeTranscriptApi.get_transcript(video_id) 
+      general_text = " ".join(i['text'] for i in dictSubtitle)
+      for i in dictSubtitle:
+        general_text += " " + i['text']
+      client = OpenAI()
+      completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=[
+      {"role": "system", "content": f"I am sending you a text.you MUST represent 10 quetion,the questions and answers as a list of dicts in Python in this way: like json file. without python``` and without numbering, only listof dicts: answers must did not have \"symbol \"qeuestion\",\"A\":answer,\"B\":answer,\"C\":answer,\"D\":answer, correct: same  answer which is correct: TEXT{general_text}"},
+        #{"role": "user", "content": f"{generalText}"}
+      ]
+      )
+ # path_file = 'C:\Future\\for git\Quiz_Scribe\q.txt' # os path join
 # print("pf",path_file)
 # if os.path.exists('q.txt'):
 #    os.remove(path_file)
 
-# with open('q.txt', "x") as f:
-#     f.write(completion.choices[0].message.content)
-#     #\text = f.read()
+      with open(file_path, "r+") as f:
+        f.write(completion.choices[0].message.content)
+        text = f.read()
 # with open('q.txt', "r") as f:
 #    text = f.read()
 # print(text)
 # def load_quiz_text():
 #    return eval(tex  
-text = completion.choices[0].message.content
-quiz_Text = eval(text) #load_quiz_text()
+      #text = completion.choices[0].message.content
+  quiz_Text = eval(text) #load_quiz_text()
 
 # TODO Tab size issue 
 
 
-for i in range(10):
-  question_text = str(i+1) + ". " + quiz_Text[i]["question"]
-  answer = quiz_Text[i]["correct"]
-  option = st.radio(label= question_text,options = (quiz_Text[i]["A"], quiz_Text[i]["B"], quiz_Text[i]["C"], quiz_Text[i]["D"])
-  )          
-  if(option == answer):
-    correct_answer+=1
-if st.button("Submit"):
-   st.write("correct_answer",correct_answer)
-
-   
-
-
+  for i in range(10):
+    question_text = str(i+1) + ". " + quiz_Text[i]["question"]
+    answer = quiz_Text[i]["correct"]
+    option = st.radio(label= question_text,options = (quiz_Text[i]["A"], quiz_Text[i]["B"], quiz_Text[i]["C"], quiz_Text[i]["D"])
+    )          
+    if(option == answer):
+      correct_answer+=1
+  if st.button("Submit"):
+     st.write("correct_answer",correct_answer)
+    
 
