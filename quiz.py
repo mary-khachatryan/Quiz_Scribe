17c52b500b99a9a43358d3b5ccd36b59ef6ccc60
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-04-20 19:01:37 +0400

    replace txt with json

--- quiz.py
+++ quiz.py
@@ -4,50 +4,87 @@ from openai import OpenAI
 from youtube_transcript_api import YouTubeTranscriptApi
 import random
 import os
+import json
+
+text = ""
 correct_answer = 0
 check =0
 
-video_id = st.text_input("Send YouTube video Id")
-num_question = int(st.text_input("How many questions Do you Want? [1,10]"))
- 
+#video_id = st.text_input("Send YouTube video Id")
+num_question =3 # int(st.text_input("How many questions Do you Want? [1,10]"))
+video_id = "J82z1hkl3Bo" #"1ooqsxtdGS0" 
+#dictSubtitle = YouTubeTranscriptApi.get_transcript(video_id)
+
+# general_text = " ".join(i['text'] for i in dictSubtitle)
+# for i in dictSubtitle:
+#   general_text += " " + i['text']
+# client = OpenAI()
+# completion = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#   {"role": "system", "content": f"Hey ChatGPT, I have a text that I'd like you to generate questions and answers for. Could you create 10 questions along with their answers based on the text? For each question, please provide the question itself, along with four possible answers labeled as A, B, C, and D. Also, indicate which answer is correct by specifying the correct answer letter. Please format the output as a JSON file. Here's the text: {general_text}"},
+#    #{"role": "user", "content": f"{generalText}"}
+#   ]
+#   ) 
+# y = json.loads(completion.choices[0].message.content)
+# with open("text.json", "w") as json_file:
+#     json.dump(y, json_file)
+# st.write(y)
+# print(type(completion.choices[0].message.content))
+# print(type(y))
+
+
+
+
+
+
+
+
+
 if st.button("next"):
-  txtfile_name = video_id + ".txt"
+  txtfile_name = video_id + ".json"
   print("here",txtfile_name)
   file_path = "C:\Future\\for git\\Quiz_Scribe\\" + txtfile_name
-  check = 1
+  
   if  os.path.exists(file_path):
-    with open(file_path, "r") as f:
-      text = f.read()
+    with open(file_path) as f:
+      text = json.load(f)
+      
   else:
       dictSubtitle = YouTubeTranscriptApi.get_transcript(video_id) 
       general_text = " ".join(i['text'] for i in dictSubtitle)
       for i in dictSubtitle:
-        general_text += " " + i['text']
+       general_text += " " + i['text']
       client = OpenAI()
       completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
-      {"role": "system", "content": f"I am sending you a text.you MUST represent 10 quetion,the questions and answers as a list of dicts in Python in this way: like json file. without python``` and without numbering, only listof dicts: answers must did not have \"symbol \"qeuestion\",\"A\":answer,\"B\":answer,\"C\":answer,\"D\":answer, correct: same  answer which is correct: TEXT{general_text}"},
-        #{"role": "user", "content": f"{generalText}"}
+      {"role": "system", "content": f"I am sending you a text.you MUST represent 10 quetion,the questions and  send answers as json file.  which qontainn keys and values like: \"qeuestion\",\"A\":answer,\"B\":answer,\"C\":answer,\"D\":answer, correct: same  answer which is correct: TEXT{general_text}"},
+          #{"role": "user", "content": f"{generalText}"}
       ]
       )
 
-      with open(file_path, "w") as f:
-        f.write(completion.choices[0].message.content)
-      with open(file_path, "r") as f:
-        text = f.read()
+      quizfile_json = json.loads(completion.choices[0].message.content)
+      with open(file_path, "w") as json_file:
+        json.dump(quizfile_json, json_file)
+      with open(file_path, "r") as json_file:
+        text = json.load(json_file)
 
-  quiz_Text = eval(text) 
+quiz_Text = text 
+st.write(quiz_Text["questions"][1]["question"])
 
 # TODO Tab size issue 
-for i in range(0,num_question):
-    question_text = str(i+1) + ". " + quiz_Text[i]["question"]
-    answer = quiz_Text[i]["correct"]
-    option = st.radio(label= question_text,options = (quiz_Text[i]["A"], quiz_Text[i]["B"], quiz_Text[i]["C"], quiz_Text[i]["D"])
+for i in range(num_question):
+    question_text = str(i+1) + ". " + quiz_Text["questions"][i]["question"]
+    answer = quiz_Text["questions"][i]["correct"]
+    option = st.radio(label= question_text,options = (quiz_Text["questions"][i]["A"], quiz_Text["questions"][i]["B"], quiz_Text["questions"][i]["C"], quiz_Text["questions"][i]["D"])
     )          
     if(option == answer):
       correct_answer+=1
 if st.button("Submit"):
      st.write("correct_answer",correct_answer)
+    
+        
+    
     
 
